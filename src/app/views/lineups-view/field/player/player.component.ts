d07fa17edd92b14212fab19e27a32d74530a3e3c
Bassam AL SAEDY <basaidi@gmail.com>
2020-07-29 11:34:54 +0200

    components restructured!

--- src/app/views/lineups-view/field/player/player.component.ts
+++ src/app/views/lineups-view/field/player/player.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.css']
+  styleUrls: ['./player.component.css'],
 })
-export class PlayerComponent implements OnInit {
+export class PlayerComponent {
+  constructor() {}
+  dragPosition = { x: 0, y: 0 };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  changePosition() {
+    this.dragPosition = {
+      x: this.dragPosition.x + 50,
+      y: this.dragPosition.y + 50,
+    };
   }
 
+  ngOnInit(): void {}
+
+  players = [
+    {
+      title: 'Name',
+      poster: 'assets/Images/Icons/player-1.png',
+    },
+  ];
+
+  drop(event: CdkDragDrop<{ title: string; poster: string }[]>) {
+    moveItemInArray(this.players, event.previousIndex, event.currentIndex);
+  }
 }
