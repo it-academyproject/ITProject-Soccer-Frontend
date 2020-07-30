f39b86c963513cc7a100bfeaff709ab64b9c087e
Bassam AL SAEDY <basaidi@gmail.com>
2020-07-30 16:46:27 +0200

    Drag & Drop updated but still not functioning yet!

--- src/app/views/lineups-view/field/player/player.component.ts
+++ src/app/views/lineups-view/field/player/player.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-player',
@@ -26,7 +30,38 @@ export class PlayerComponent {
     },
   ];
 
+  // drop(event: CdkDragDrop<{ title: string; poster: string }[]>) {
+
+  //   moveItemInArray(this.players, event.previousIndex, event.currentIndex);
+  // }
+
   drop(event: CdkDragDrop<{ title: string; poster: string }[]>) {
-    moveItemInArray(this.players, event.previousIndex, event.currentIndex);
+    if (event.previousContainer !== event.container) {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      moveItemInArray(this.players, event.previousIndex, event.currentIndex);
+    }
   }
+
+  // drop(event: CdkDragDrop<string[]>) {
+  //   if (event.previousContainer === event.container) {
+  //     moveItemInArray(
+  //       event.container.data,
+  //       event.previousIndex,
+  //       event.currentIndex
+  //     );
+  //   } else {
+  //     transferArrayItem(
+  //       event.previousContainer.data,
+  //       event.container.data,
+  //       event.previousIndex,
+  //       event.currentIndex
+  //     );
+  //   }
+  // }
 }
